de087fe4abde0112ceaee382bf26554b5f31a382
leslie alvarado <alvarado.leslie12@yahoo.com>
2024-04-30 14:01:09 -0700

    Updates functions in js
    Deleted validations in functions

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 "use strict";
 
 function buttonConverter() {
-
-  // This stores the data as a string of text
   //The text of the input will be stored in the variable myNumber and then displayed in the alert
   let myNumber = Number(document.getElementById("myNumber").value);
-  const messageError = document.getElementById("messageError");
-
-  //Algorithm calculation
 
   // Celsius=(Fahrenheit−32)× 5/9
-  let result = (myNumber - 32) * (5 / 9);
+  let result = ((myNumber - 32) * (5 / 9)).toFixed(2);
+
+  displayData(result);
+
+  //if (isNaN(myNumber)) {
+  // messageError.innerHTML = "One or more of your input values are invalid.";
+  // return; // exit the function   }
+  // clear any previous error message
+  //messageError.innerHTML = "";
+}
+
+function buttonConverter2() {
+  // The text of the input will be stored in the variable myNumber and then displayed in the alert
+  let myNumber = Number(document.getElementById("myNumber").value);
+
+  // Fahrenheit=(Celsius * 9/5) +32
+  let result = ((myNumber * 9 / 5) + 32).toFixed(2);
+  displayData(result);
+}
+
+function displayData(_result) {
   //Display it in element, on the screen so we use innerhtml
   document.getElementById("result").innerHTML =
-    "<span class='bold-large'>" + result + "</span>";
-
-  if (isNaN(myNumber)) {
-    messageError.innerHTML = "One or more of your input values are invalid.";
-    return; // exit the function
-  }
-  // clear any previous error message
-  messageError.innerHTML = "";
+    "<h1><span class='bold-large'>" + _result + "</h1></span>";
 }
